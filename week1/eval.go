6c5a447aa27271d7eb1e0bcb7f476bd7ed516b05
Khoa Bui Hoang Dang <khoa.bui@ITVN001290-MAC.local>
2019-07-19 13:15:02 +0700

    cover more cases and catch errors

--- week1/eval.go
+++ week1/eval.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 func cleanSpaces(s string) string {
-    return strings.Join(strings.Fields(s), " ")
+	return strings.Join(strings.Fields(s), " ")
 }
 
-func eval(text string) (result float64) {
-    args := strings.Split(cleanSpaces(text), " ")
-    x, _ := strconv.ParseFloat(args[0], 64)
-    y, _ := strconv.ParseFloat(args[2], 64)
-    op := args[1]
+func eval(text string) (result float64, err error) {
+	args := strings.Split(cleanSpaces(text), " ")
+	if len(args) < 3 {
+		return 0, errors.New("Please provide arguments in the right format <1st num> <arithmetic> <2nd num>")
+	}
+	x, err := strconv.ParseFloat(args[0], 64)
+	if err != nil {
+		return
+	}
+	y, err := strconv.ParseFloat(args[2], 64)
+	if err != nil {
+		return
+	}
+	op := args[1]
 
-    switch op {
-        case "+":
-            result =  x + y
-        case "-":
-            result = x - y
-        case "*":
-            result = x * y
-        case "/":
-            result = x / y
-        default:
-            fmt.Println("Invalid arguments")
-    }
-    return result
+	switch op {
+	case "+":
+		result = x + y
+	case "-":
+		result = x - y
+	case "*":
+		result = x * y
+	case "/":
+		result = x / y
+	default:
+		fmt.Println("Invalid arguments")
+	}
+	return
 }
--- week1/main.go
+++ week1/main.go
@@ -7,11 +7,19 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+    scanner := bufio.NewScanner(os.Stdin)
+    fmt.Println("Format: <1st num> <arithmetic> <2nd num>")
 	fmt.Print("> ")
 
 	for scanner.Scan() {
-        fmt.Println(eval(scanner.Text()))
+        res, err := eval(scanner.Text())
+        if err != nil {
+        	fmt.Println(err.Error())
+			fmt.Print("> ")
+        	continue
+		} else {
+			fmt.Println(res)
+		}
         fmt.Print("> ")
     }
 }
